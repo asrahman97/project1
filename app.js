d099182c07950e097fcbde6e54246fd647bf2469
asrahman97 <asrahman97@gmail.com>
2019-11-25 09:15:45 -0500

    commit for class

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 window.onload = function () {
-  
+
+
+  const startButton = document.querySelector('.start-btn')
+  const nextButton = document.querySelector('.next-btn')
 
   async function sample() {
     let results = await axios.get("https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/players")
 
-  
+
     console.log(results)
+    startButton.addEventListener('click', function () {
+      console.log('Start button clicked')
+    })
   }
 
   sample();
+
+
+  const multipleChoice = [
+    {
+      question: 'What team does (key) play for?',
+      answers: [
+        { text: 'false', correct: false },
+        { text: 'key', correct: true }
+      ]
+    },
+
+    {
+      question: 'What conference does (key) play in?',
+      answers: [
+        { text: 'key', correct: true },
+        { text: 'false', correct: false }
+      ]
+    },
+
+    
+    
+  ]
+
 }
 
