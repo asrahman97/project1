935305e5d969ecac4fda00e252cc67c9b0f76042
asrahman97 <asrahman97@gmail.com>
2019-11-25 10:58:51 -0500

    added objects in question array

--- app.js
+++ app.js
@@ -3,19 +3,37 @@ window.onload = function () {
 
   const startButton = document.querySelector('.start-btn')
   const nextButton = document.querySelector('.next-btn')
+  const questionBox = document.querySelector('#question-container')
+  const questionElement = document.querySelector('#question')
+  const answerElement = document.querySelector('#answer-buttons')
+  let currentQuestion = 0;
+  let score = 0; 
+  
 
   async function sample() {
     let results = await axios.get("https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/players")
 
 
     console.log(results)
-    startButton.addEventListener('click', function () {
-      console.log('Start button clicked')
-    })
+    startButton.addEventListener('click', startQuiz)
   }
 
   sample();
 
+  function startQuiz() {
+    console.log('Game started')
+    startButton.classList.add('hide')
+    questionBox.classList.remove('hide')
+    nextQuestion()
+  }
+
+  function nextQuestion(multipleChoice) {
+    questionElement.innerText = multipleChoice.question
+  }
+
+  function selectChoice() {
+
+  }
 
   const multipleChoice = [
     {
@@ -34,7 +52,31 @@ window.onload = function () {
       ]
     },
 
-    
+    {
+      question: 'What division do the (key) play in?',
+      answers: [
+        { text: 'key', correct: true },
+        { text: 'false', correct: false }
+      ]
+    },
+
+    {
+      question: 'What conference does (key) play in?',
+      answers: [
+        { text: 'key', correct: true },
+        { text: 'false', correct: false }
+      ]
+    },
+
+    {
+      question: 'What team does (key) play for?',
+      answers: [
+        { text: 'false', correct: false },
+        { text: 'key', correct: true }
+      ]
+    }
+
+
     
   ]
 
